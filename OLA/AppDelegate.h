45d3099c83153bca5e50c9bc86dcfdae364bb6dd
Kiran <kiran@prazas.com>
2015-02-07 10:33:04 +0530

    OLA
    OLA

---
+++ OLA/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OLA
+//
+//  Created by Kiran Juikar on 07/02/15.
+//  Copyright (c) 2015 Blackhat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OLA/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OLA
+//
+//  Created by Kiran Juikar on 07/02/15.
+//  Copyright (c) 2015 Blackhat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
