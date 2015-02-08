2f847f5b5fba11b5480ce46127a6dc71beb9a6a8
Kiran <kiran@prazas.com>
2015-02-08 10:42:05 +0530

    Corrected cloud code
    Corrected cloud code

--- OLA/main.js
+++ OLA/main.js
@@ -9,54 +9,97 @@ Parse.Cloud.define("validatesms", function(request, response) {
       query.each(function(user) {
       // Update to plan value passed in
         var dbcode = user.get("code");
-        
+        var driverName = user.get("name");
+        var vehicle = user.get("vehicleNO");
         if(dbcode === smscode){
+            client.sendSms({
+                to:'+919420393906', 
+                from: '+15017258260', 
+                body: 'Congratulations!!! Your OLA booking is confirmed. Driver Name: ' + driverName + " Vehicle No: " + vehicle + ". Have a safe ride."
+              }, function(err, responseData) { 
+                if (err) {
+                  console.log(err);
+                } else { 
+                }
+              });
             result = true;
         }
       });
         
     response.success(result);
-
 });
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example: 
 Parse.Cloud.define("sendsms", function(request, response) {
-  
-  var driverusername = request.params.username;
-  var text = "";
+    
+    var driverusername = request.params.username;
+    var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     for( var i=0; i < 6; i++ )
         text += possible.charAt(Math.floor(Math.random() * possible.length));
+        
+    // Require and initialize the Twilio module with your credentials
+var client = require('twilio')('AC2bdff5e9b62b8cf33abc24cf5d06128a', '930607ffb1eb70754232c7f1dc60808a');
+ 
+// Send an SMS message
+client.sendSms({
+    to:'+919420393906', 
+    from: '+15017258260', 
+    body: "Your OLA booking code is: " + text + " Please share it with driver & you will get confirmation SMS with driver details."
+  }, function(err, responseData) { 
+    if (err) {
+      console.log(err);
+    } else { 
+        Parse.Cloud.useMasterKey();
+        var query = new Parse.Query("SMSCode");
+        query.equalTo("driver_username", driverusername);
+          query.each(function(user) {
+          // Update to plan value passed in
+          user.set("code", text);
+          user.save(); 
+          response.success(user);
+        });
+    }
+  }
+);
 
-    // Set up to modify user data
-    
   
-    Parse.Cloud.httpRequest({
-        method: 'POST',
-        url: 'http://api.mVaayoo.com/mvaayooapi/MessageCompose',
-        body: {
-          user: 'k.juikar12@gmail.com:Swami@123',
-          receipientno: '9881234950',
-          senderID: 'OLA Cabs',
-          msgtxt: 'text'
-        },
-        success: function(httpResponse) {
-          //response.success(httpResponse.text);
-          Parse.Cloud.useMasterKey();
-          var query = new Parse.Query("SMSCode");
-          query.equalTo("driver_username", driverusername);
-            query.each(function(user) {
-            // Update to plan value passed in
-            user.set("code", text);
-            user.save(); 
-            response.success(user);
-            });
-        },
-        error: function(httpResponse) {
-          response.success('Request failed with response code ' + httpResponse.status);
-        }
-    });
+//  var driverusername = request.params.username;
+//  var text = "";
+//    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+//
+//    for( var i=0; i < 6; i++ )
+//        text += possible.charAt(Math.floor(Math.random() * possible.length));
+//
+//    // Set up to modify user data
+//    
+//  
+//    Parse.Cloud.httpRequest({
+//        method: 'POST',
+//        url: 'http://api.mVaayoo.com/mvaayooapi/MessageCompose',
+//        body: {
+//          user: 'k.juikar12@gmail.com:Swami@123',
+//          receipientno: '9881234950',
+//          senderID: 'OLA Cabs',
+//          msgtxt: 'text'
+//        },
+//        success: function(httpResponse) {
+//          //response.success(httpResponse.text);
+//          Parse.Cloud.useMasterKey();
+//          var query = new Parse.Query("SMSCode");
+//          query.equalTo("driver_username", driverusername);
+//            query.each(function(user) {
+//            // Update to plan value passed in
+//            user.set("code", text);
+//            user.save(); 
+//            response.success(user);
+//            });
+//        },
+//        error: function(httpResponse) {
+//          response.success('Request failed with response code ' + httpResponse.status);
+//        }
+//    });
 });
 
 
